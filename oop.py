2f73465006246f0bd00636b6a4779f78948844d3
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-04 00:41:52 +0600

    third commit

--- oop.py
+++ oop.py
@@ -4,16 +4,30 @@ class  MyClass:
 p1= MyClass()
 print(p1.x)
 
-"""
+#2
 class Person:
-    def_int_(self,name,age):
+  def __init__(self, name, age):
     self.name = name
     self.age = age
 
-    def myfunc(self):
-        print("HEllo my name is " + self.name)
+  def myfunc(self):
+    print("Hello my name is " + self.name)
 
-p1= Person("Jhon",35)
+p1 = Person("John", 36)
+
 del p1
 
-print (p1)
+print(p1)
+"""
+
+#3
+
+class MyClass:
+    x= 5
+p1=MyClass()
+p2=MyClass()
+p3=MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
