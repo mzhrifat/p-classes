53911a295fa4ae8112484b1d550ac5916ed6966e
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-04 00:38:11 +0600

    second commit

--- oop.py
+++ oop.py
@@ -1,4 +1,19 @@
+"""
 class  MyClass:
     x=5
-p1= MyClass
+p1= MyClass()
 print(p1.x)
+
+"""
+class Person:
+    def_int_(self,name,age):
+    self.name = name
+    self.age = age
+
+    def myfunc(self):
+        print("HEllo my name is " + self.name)
+
+p1= Person("Jhon",35)
+del p1
+
+print (p1)
