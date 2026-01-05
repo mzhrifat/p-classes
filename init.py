d0d63b696f59b25bf6a98d5c2c83b876d0ffd460
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-05 21:16:29 +0600

    init student commit

--- init.py
+++ init.py
@@ -1,3 +1,4 @@
+"""
 class Person:
   def __init__(self, name, age):
     self.name = name
@@ -7,3 +8,16 @@ p1 = Person("Emil", 36)
 
 print(p1.name)
 print(p1.age)
+"""
+
+class Student:
+  def __init__(self, name, age=18):
+    self.name = name
+    self.age = age
+
+p1 = Student("Emil")
+p2 = Student("Tobias", 25)
+
+
+print(p1.name,p1.age)
+print(p2.name,p2.age)
