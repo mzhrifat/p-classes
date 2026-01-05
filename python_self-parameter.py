a5855b30f63bd108190bab179b3c2bb1388f6de2
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-05 22:43:29 +0600

    self changed commit

--- python_self-parameter.py
+++ python_self-parameter.py
@@ -1,3 +1,4 @@
+"""
 class Person:
   def __init__(self, name, age):
     self.name = name
@@ -7,4 +8,18 @@ class Person:
     print("Hello, my name is " + self.name)
 
 p1 = Person("Emil", 25)
+p1.greet()
+"""
+
+class Person:
+    def __init__ (myobject,name,age):
+        myobject.name = name
+        myobject.age = age
+
+    def greet(Wellcome):
+        print("Helo,my name is"+ Wellcome.name)
+
+p1=Person("Rif")
+p1= Person(32)
+
 p1.greet()
