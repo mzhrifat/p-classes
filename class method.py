d6c47b0f6e2dec65a33076f692654d4b0664c185
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-06 20:12:59 +0600

    add and multiply commit

--- class method.py
+++ class method.py
@@ -1,3 +1,4 @@
+"""
 class Person:
     def __init__(self,name):
         self.name=name
@@ -7,3 +8,17 @@ class Person:
 
 p1=Person("Emi")
 p1.greet()
+"""
+#make a calculator
+
+class Calculator:
+    def add(self,a,b):
+        return a+b
+    
+    def multiply(self,a,b):
+        return a*b
+
+calc= Calculator()
+print(calc.add(10,4))
+print(calc.multiply(6,5))
+
