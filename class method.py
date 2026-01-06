ed53f9feb2221d641f349d11d01a06add4fc3113
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-06 20:24:13 +0600

    accesing code commit

--- class method.py
+++ class method.py
@@ -8,7 +8,7 @@ class Person:
 
 p1=Person("Emi")
 p1.greet()
-"""
+
 #make a calculator
 
 class Calculator:
@@ -22,3 +22,15 @@ calc= Calculator()
 print(calc.add(10,4))
 print(calc.multiply(6,5))
 
+"""
+#Methods Accessing Properties
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def get_info(self):
+        return  f"{self.name} is {self.age} years old"
+
+p1=Person("AS" ,33)
+print(p1.get_info())
