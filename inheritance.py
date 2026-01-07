aeef79d95971848b5c87a00646b4a18b91cbbb51
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-07 18:54:49 +0600

    add a init commit

--- inheritance.py
+++ inheritance.py
@@ -11,7 +11,7 @@ class Person:
 ##Use the Person class to create an object, and then execute the printname method:
 x1=Person("Jo","Ha")
 x1.printname()
-"""
+
 #Create a child class
 
 class Person:
@@ -26,3 +26,21 @@ class Student(Person):
 
 x=Student("HJ","gf")
 x.printname()
+"""
+
+#ADD a __init__ () function
+class Person:
+    def __init__(self,fname,lname):
+        self.firstname=fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname,self.lastname)
+
+class Student(Person):
+    def __init__(self,fname,lname):
+        Person.__init__(fname,lname)
+
+x=Student("MIKe","Koly")
+x.printname()
+        
