9154fef9e0626caebccf786f12a211087e5b013a
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-07 18:32:53 +0600

    INheritance commit

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__ (self,fname,lname):
+        self.firstname=fname
+        self.lastname=lname
+
+    def printname(self):
+        print(self.firstname,self.lastname)
+
+##Use the Person class to create an object, and then execute the printname method:
+x1=Person("Jo","Ha")
+x1.printname()
