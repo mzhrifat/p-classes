311093e0f196ed199b7c47f2f626318c864355a4
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-09 00:54:55 +0600

    real life 4 commit

--- Polymorphism.py
+++ Polymorphism.py
@@ -35,7 +35,7 @@ thisdict= {
 }
 
 print(len(thisdict))
-"""
+
 #Different classes with the same method:
 
 class Car:
@@ -67,3 +67,34 @@ plane1=Plane("Boeing","747")
  
 for x in (car1,boat1,plane1):
     x.move()
+"""
+#Create a class called Vehicle and make Car, Boat, Plane child classes of Vehicle
+
+class Vechicle:
+    def __init__(self,brand,model):
+        self.brand=brand
+        self.model=model
+
+    def move(self):
+        print("Move!")
+class Car(Vechicle):
+    pass
+
+class Boat(Vechicle):
+    def move(self):
+        print("Sail!")
+
+class Plane(Vechicle):
+    def move(self):
+        print("Fly!")
+
+car1=Car("Ford","Mustang")
+boat1=Boat("Ibiza","Touring")
+plane1=Plane("boeing","747")
+
+
+for x in (car1,boat1,plane1):
+    print(x.brand)
+    print(x.model)
+    x.move()
+   
--- real life.py
+++ real life.py
@@ -82,7 +82,7 @@ my_playlist.play_song("Hotel California")
 
 my_playlist.shuffle_playlist()
 my_playlist.show_songs()
-"""
+
 #payment method
 
 class Payment():
@@ -109,3 +109,32 @@ Payments=[CreditCard(),Bkash(),Nagad(),Rocket()]
 amount=100
 for Payment in Payments:
     Payment.pay(amount)        
+"""
+
+class Vecicle:
+    def __init__(self,brand,model):
+        self.brand=brand
+        self.model=model
+    
+    def move(self):
+        print("Move!")
+
+class Car(Vecicle):
+    pass
+
+class Boat(Vecicle):
+    def move (self):
+       print("Sail!")
+
+class Plane(Vecicle):
+    def move(self):
+        print("Fly!")
+
+car1=Car("ca","fa")
+boat1=Boat("daa","da")
+plane1=Plane("ajaj","999")
+
+for x in (car1, boat1, plane1):
+   print(x.brand)
+   print(x.model)
+   x.move()
