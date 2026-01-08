150c0fa606c04b7b84dd4564a67b91a4c6778273
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-08 23:50:56 +0600

    chat er example commit

--- Polymorphism.py
+++ Polymorphism.py
@@ -1,9 +1,28 @@
 """
-
 x=("Apple","banana","cherry")
+print(len(x))
 
+#2
+x= "Hello World"
 print(len(x))
 """
 
-x= "Hello World"
-print(len(x))
+class animal:
+    def sound(self):
+        print("Animal makes Sounds")
+
+class dog(animal):
+    def sound(self):
+        print ("dog Barks")
+
+class cat(animal):
+    def sound(self):
+        print("cat meows")
+
+a=animal()
+d=dog()
+c=cat()
+
+a.sound()
+d.sound()
+c.sound()
