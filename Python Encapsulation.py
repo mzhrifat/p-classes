86f2014527d7a9cb9224cad761e5d4a8c65405fb
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-09 23:25:03 +0600

    Use a setter method to change a private property commit

--- Python Encapsulation.py
+++ Python Encapsulation.py
@@ -1,3 +1,4 @@
+"""
 #Get Private Property Value
 
 class Person:
@@ -10,3 +11,28 @@ class Person:
     
 p1=Person("Tobias",25)
 print(p1.get_age())
+"""
+
+#Use a setter method to change a private property
+
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.__age=age
+
+    def get_age(self):
+        return self.__age
+    
+    def set_age(self,age):
+        if age>0:
+            self.__age = age
+        else:
+            print("Age must be positive")
+
+p1=Person("AN",21)
+print(p1.get_age())
+
+p1.set_age(26)
+print(p1.get_age())
+
+    
