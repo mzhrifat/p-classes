041fdd440db90133230c45f0e816c047f9c45b4f
MD.ZIAUL HASHAN RIFAT <mzhripat@gmail.com>
2026-01-11 00:12:13 +0600

    outer inner commit

--- python inner class.py
+++ python inner class.py
@@ -1,6 +1,9 @@
+"""
+
 class Outer:
     def __init__(self):
-        self.name="Outer class"
+        self.name="Outer"
+    
     class Inner:
         def __init__(self):
             self.name = "Inner Class"
@@ -9,4 +12,22 @@ class Outer:
             print("This is the inner class")
 
 outer= Outer()
-print(outer.name)
+inner = outer.Inner()
+inner.display()
+"""
+
+#Pass the outer class instance to the inner class
+class Outer:
+    def __init__(self):
+        self.name = "Emil"
+
+    class Inner:
+        def __init__(self,outer):
+            self.outer = outer
+
+        def display(self):
+             print(f"Outer class name: {self.outer.name}")
+
+outer = Outer()
+inner = outer.Inner(outer)
+inner.display()
